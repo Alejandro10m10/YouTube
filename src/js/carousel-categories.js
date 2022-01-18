011d0ab419d4a36b339fe923901b994c749e0b8f
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2022-01-17 23:17:36 -0600

    Comenzando con la translación de elementos de la barra de navegación por categoria

--- src/js/carousel-categories.js
+++ src/js/carousel-categories.js
@@ -2,17 +2,31 @@ const btnPrevious = document.querySelector('#btnPreviousCateogry'),
       btnNext = document.querySelector('#btnNextCategory'),
       categoriesContent = document.querySelector('.right-nav__ul__li');
 
-let arrayCategories = ["Todos", "Música", "Deportes", "Belleza", "Mascotas", "Ciencia", "Cine", "Fútbol", "Gamer", "Viajes", "Vlogs", "Unboxing",
-"Tecnología", "Steamer", "Noticias"];
+let arrayCategories = ["Todos", "Música", "Deportes", "Belleza", "Mascotas", "Ciencia", "Cine", "Fútbol", "Gamer", "Viajes", "Vlogs", 
+            "Unboxing", "Tecnología", "Steamer", "Noticias", "Pódcast", "En tiempo real","Subidos recientemente", "Novedad para ti"];
 
 makeButtons();
 
-btnNext.addEventListener('click', nextCategory);
+let xTranslate = 0;
+const pxTranslate = 360;
 
+
+btnNext.addEventListener('click', nextCategory);
+btnPrevious.addEventListener('click', previusCategory);
+
+function contentFunction(){
+
+}
 function nextCategory(){
     hidePreviousButton(true);
     showBorderBackground(true);
     translateElements("right");
+}
+
+function previusCategory(){
+    hidePreviousButton(true);
+    showBorderBackground(true);
+    translateElements("left");
 }
 
 function hidePreviousButton(value){
@@ -41,14 +55,16 @@ function showBorderBackground(value){
     }
 }
 
-function translateElements(transale){
-    if(transale === "right"){
-        let categoriesList = categoriesContent.childNodes;
-        let translate = 100;
-        for(let i = 1; i < categoriesList.length ; i++){
-            categoriesList[i].style.transform = "translateX(`{translate}`px)";
-        }
-    } else{
+function translateElements(translate){
 
+    (translate === "right") 
+        ? xTranslate += pxTranslate
+        : xTranslate -= pxTranslate;
+
+    let categoriesList = categoriesContent.childNodes;
+    for(let i = 1; i < categoriesList.length ; i++){
+        categoriesList[i].style.transform = `translateX(-${xTranslate}px)`;
     }
+    
+
 }
